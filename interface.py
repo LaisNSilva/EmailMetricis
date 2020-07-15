a49f629b78dae007c72b718166c2fc5636bfa860
F.Fincatti <fernandocfbf@al.insper.edu.br>
2020-07-15 20:04:59 -0300

    interface

---
+++ interface.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 15 19:33:40 2020
+
+@author: Fernando
+"""
+from tkinter import *
+import pandas as pd
+from tkinter import * 
+from tkinter.ttk import *
+  
+# importing askopenfile function 
+# from class filedialog 
+from tkinter.filedialog import askopenfile 
+
+root = Tk() 
+root.geometry('200x100') 
+  
+# This function will be used to open 
+# file in read mode and only Python files 
+# will be opened 
+def open_file(): 
+    file = askopenfile(mode ='r', filetypes =[('Excel Files', '*.xlsx')]) 
+    if file is not None: 
+        file_name = file.name
+        print(file_name)
+        content = pd.read_excel(file_name) 
+        print(content) 
+  
+btn = Button(root, text ='Open', command = lambda:open_file()) 
+btn.pack(side = TOP, pady = 10) 
+  
+mainloop() 
