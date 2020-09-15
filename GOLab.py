f5c3a7b34c6be8de51ab066d623e873bc4bb85d5
LaisNSilva <silva.lais72@hotmial.com>
2020-09-14 22:09:47 -0300

    tentativa classificar html

--- GOLab.py
+++ GOLab.py
@@ -4,7 +4,31 @@ Created on Sun Aug 30 11:59:35 2020
 
 @author: Lais Nascimento
 """
+from interface import *
+from tkinter import *
+import pandas as pd 
+from tkinter.ttk import *
+import re
+from textblob import TextBlob
+from tqdm.auto import tqdm
+from tkinter import Label
+import time
 
+
+
+# Importar a função que classifica as manchetes
+#from funcao.ipynb import classificar
+  
+# importing askopenfile function 
+# from class filedialog 
+from tkinter.filedialog import askopenfile
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn.metrics import f1_score, recall_score
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_score
 
 
 from bs4 import BeautifulSoup
@@ -93,8 +117,85 @@ tentativa(1000, "#ngo > div.ngo-popup > div.got-it")
 
 lista_de_projetos = acha_lista(1000, "#ngo > div.search-results > div")
 
+def cleanup(text):
+    import string
+    punctuation = '[\/!-.:?;]' # Note que os sinais [] são delimitadores de um conjunto.
+    pattern = re.compile(punctuation)
+    text_subbed = re.sub(pattern, ' ', text)
+    return text_subbed
+
+def classifica_dataset(dataset, model, count_vect):
+    X = dataset.loc[:,"Manchete"]
+    pred = count_vect.transform(X).toarray()
+    predict = model.predict(pred)
+
+    dataset["Relevância"] = pd.Series(predict)
+
+    return dataset
+
+def classificar (treino, arquivo):
+    global relevantes
+    
+    progress['value'] = 0
+    root.update_idletasks() 
+    time.sleep(1) 
+    
+    X_para_treinar = treino.loc[:, "Manchete"]
+    Y_para_treinar = treino.loc[:, "Relevância"]
+    ##y = arquivo.loc[:, "Relevância"]
+
+    # Vetorizando
+    count_vect = CountVectorizer()
+    X_train_counts = count_vect.fit_transform(X_para_treinar).toarray()
 
 
+    # Sepando em teste e treino
+    # Porém vou colocar tudo para treino
+    X_train, X_test, y_train, y_test = train_test_split(X_train_counts, Y_para_treinar, test_size=0.001, random_state=42)
+    #Aplicando o modelo SVM (Support Vector Machine)
+
+    kernels = ['linear', 'poly', 'rbf', 'sigmoid', 'precomputed']
+
+    model = svm.SVC(kernel = 'linear') #Kernel
+
+    #Dando fit no modelo SVM usando o dataset de treino
+    model.fit(X_train, y_train)
+    
+    planilha_classificada = classifica_dataset(dataset_escolhido_preparado, model, count_vect)
+    
+base_para_treinar = pd.read_excel("base_pronta.xlsx")    
+
+manchetes = []
 for elemento in lista_de_projetos:
-    print(elemento.text + " ------------------------------ " + str(type(elemento)))
+    manchetes.append(elemento.text)
+print(manchetes)
+
+relevantes = pd.read_excel("ManchetesRelevantes_GOLab.xlsx")
+
+# POR ENQUATO
+links = []
+n=0
+for e in manchetes:
+    n+=1
+    links.append(n)
+    
+dicionario = {}
+
+dicionario['Manchetes'] = lista_manchetes
+#dicionario['Indice'] = email
+dicionario['Link'] = lista_links
+
+y_pred = []
+for dado in X_novo:
+    n_dado=cleanup(dado)
+    previsao = preve(clf, model, dtc, model_lg, n_dado)
+    y_pred.append(previsao)
+
+data_manchetes = pd.DataFrame(data=dicionario)
+
+
+
+    
+
+
 
